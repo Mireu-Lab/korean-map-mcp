49ae5cedd07787f720fb9c2a448694fb40e281ff
Mireu Lim <limmireu1214@gmail.com>
2025-09-04 21:35:57 +0000

    🐛 fix(handler): handle error when creating request
    - 요청 생성 실패 시 에러 로깅 추가
    - 응답 상태, 헤더, 본문 디버깅 로깅 추가
    - SSE 요청 프록시 URL 로깅 추가

--- app/lib/handler.go
+++ app/lib/handler.go
@@ -19,12 +19,18 @@ func (h *ApiHandler) ProxyKakaoRequestStream(w http.ResponseWriter, r *http.Requ
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
+	var err error
+
 	targetURL := kakaoAPIURL + path + "?" + r.URL.RawQuery
 	slog.Info("Proxying SSE request", "url", targetURL)
 
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", targetURL, nil)
+	req, err := http.NewRequest("GET", targetURL, nil)
 	req.Header.Set("Authorization", "KakaoAK "+os.Getenv("KAKAO_API_KEY"))
+	if err != nil {
+		slog.Error("Failed to create request", "error", err)
+		return
+	}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -32,8 +38,11 @@ func (h *ApiHandler) ProxyKakaoRequestStream(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	defer resp.Body.Close()
+	slog.Debug("Kakao API response status", "status", resp.Status)
+	slog.Debug("Kakao API response headers", "headers", resp.Header)
 
 	body, _ := io.ReadAll(resp.Body)
+	slog.Debug("Kakao API response body", "body", string(body))
 
 	fmt.Fprintf(w, "data: %s\n\n", body)
 	flusher, _ := w.(http.Flusher)
